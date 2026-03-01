7a4a4a90c9d0df45375dc2012352f33573a61686
DsRase <dsrase201@gmail.com>
2026-03-01 18:53:05 +0300

    fix playbook & db syntax

--- src/db.py
+++ src/db.py
@@ -15,7 +15,7 @@ class User(Base):
     username: Mapped[str]
     description: Mapped[Optional[str]]
 
-DB_URL = f"postgresql+psycopg2://{getenv("DB_USER")}:{getenv("DB_PASS")}@{getenv("DB_HOST")}:5432/{getenv("DB_NAME")}"
+DB_URL = f"postgresql+psycopg2://{getenv('DB_USER')}:{getenv('DB_PASS')}@{getenv('DB_HOST')}:5432/{getenv('DB_NAME')}"
 engine = create_engine(DB_URL)
 
 Base.metadata.create_all(engine)
